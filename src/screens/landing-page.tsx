ec45994d37312ba85638c56c37ac2542de7daedb
ben.azevedo <ben.azevedo@metergroup.com>
2024-10-13 10:41:05 -0700

    Initial commit

---
+++ src/screens/landing-page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './landing-page.css';
+
+function LandingPage() {
+  return (
+    <div className="landing-page-container">
+      <div className="life">L.I.F.E.</div>
+      <div className="press-start">
+        <span className="greater-than">&gt;</span> PRESS START
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
